8f05120159704feb2fd920cf55a652852dec65a8
KAMEKID <muhamadazka.hydra@gmail.com>
2024-06-10 13:01:22 +0700

    Responsi

---
+++ Responsi/live_coding_1.cpp
@@ -0,0 +1,42 @@
+// 1. SEQUENTIAL SEARCH
+
+#include <iostream>
+
+using namespace std;
+
+void sequential_search(int data[], int length, int cari) {
+    bool b_flag = false;
+    for(int i = 0; i < length; i++) {
+        if(data[i] == cari) {
+            b_flag = true;
+            cout << "\nData ditemukan di indeks ke-" << i << endl;
+            break;
+        }
+    }
+    if(!b_flag) {
+        cout << "\nData tidak ditemukan" << endl;
+    }
+}
+
+int main() {
+    int data[6] = {1, 2, 3, 4, 5, 5};
+    int length = sizeof(data) / sizeof(data[0]);
+    int cari;
+
+    cout << "\t ======= Sequential Search ======= " << endl;
+
+    cout << endl;
+
+    for(int i = 0; i < length; i++){
+        cout << data[i] << " ";
+    }
+
+    cout << endl;
+
+    cout << "Masukkan data yang ingin anda cari: ";
+    cin >> cari;
+
+    sequential_search(data, length, cari);
+
+    return EXIT_SUCCESS;
+}
